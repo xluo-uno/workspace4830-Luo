0996a4021be940c8d2255e4b6edf43314f259a96
bb <bb@Cooluo>
2021-02-03 19:22:12 -0600

    bug fix

--- webproject/src/gitTest.java
+++ webproject/src/gitTest.java
@@ -27,7 +27,7 @@ public class gitTest extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: abc").append(request.getContextPath());
+		response.getWriter().append("Hello World");
 	}
 
 	/**
